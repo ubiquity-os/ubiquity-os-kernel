e379d71f52692105438cd3e187f4b645628e0076
whilefoo <whilefoo@users.noreply.github.com>
2024-01-07 11:36:30 +0100

    feat: working POC

---
+++ src/proxy.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import SmeeClient from "smee-client";
+dotenv.config({ path: ".dev.vars" });
+
+const smee = new SmeeClient({
+	source: process.env.WEBHOOK_PROXY_URL || "https://smee.io/new",
+	target: "http://localhost:8787/events",
+	logger: console,
+});
+
+smee.start();
--- src/worker.ts
+++ src/worker.ts
@@ -1,46 +1,125 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-import { Webhooks, createNodeMiddleware } from "@octokit/webhooks";
+import { EmitterWebhookEventName, Webhooks, emitterEventNames } from "@octokit/webhooks";
 import { handleEvent } from "./webhooks";
 import { GitHubEvent } from "./types/github-events";
+import { Type as T, type Static } from "@sinclair/typebox";
+import { Value } from "@sinclair/typebox/value";
 
-export interface Env {
-	WEBHOOK_SECRET: string;
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
+const envSchema = T.Object({
+	WEBHOOK_SECRET: T.String(),
+});
+
+type Env = Static<typeof envSchema>;
 
 export default {
 	async fetch(request: Request, env: Env): Promise<Response> {
+		if (!Value.Check(envSchema, env)) {
+			const errors = [...Value.Errors(envSchema, env)];
+			console.error("Invalid environment variables", errors);
+			return new Response(
+				JSON.stringify({
+					error: "Invalid environment variables",
+				}),
+				{ status: 500, headers: { "content-type": "application/json" } }
+			);
+		}
+
+		let pathname;
+		try {
+			pathname = new URL(request.url, "http://localhost").pathname;
+		} catch (error) {
+			return new Response(
+				JSON.stringify({
+					error: `Request URL could not be parsed: ${request.url}`,
+				}),
+				{ status: 422, headers: { "content-type": "application/json" } }
+			);
+		}
+
+		if (pathname !== "/events" || request.method !== "POST") {
+			return new Response(
+				JSON.stringify({
+					error: `Unknown route: ${request.method} ${request.url}`,
+				}),
+				{ status: 400, headers: { "content-type": "application/json" } }
+			);
+		}
+
+		if (!request.headers.get("content-type") || !request.headers.get("content-type")?.startsWith("application/json")) {
+			return new Response(
+				JSON.stringify({
+					error: `Unsupported "Content-Type" header value. Must be "application/json"`,
+				}),
+				{ status: 415, headers: { "content-type": "application/json" } }
+			);
+		}
+
+		const eventName = request.headers.get("x-github-event");
+		if (!eventName) {
+			return new Response(
+				JSON.stringify({
+					error: `Missing "x-github-event" header`,
+				}),
+				{ status: 400, headers: { "content-type": "application/json" } }
+			);
+		}
+		const signatureSHA256 = request.headers.get("x-hub-signature-256");
+		if (!signatureSHA256) {
+			return new Response(
+				JSON.stringify({
+					error: `Missing "x-hub-signature-256" header`,
+				}),
+				{ status: 400, headers: { "content-type": "application/json" } }
+			);
+		}
+		const id = request.headers.get("x-github-delivery");
+		if (!id) {
+			return new Response(
+				JSON.stringify({
+					error: `Missing "x-github-delivery" header`,
+				}),
+				{ status: 400, headers: { "content-type": "application/json" } }
+			);
+		}
+
+		if (!emitterEventNames.includes(eventName as EmitterWebhookEventName)) {
+			return new Response(
+				JSON.stringify({
+					error: `Unsupported "x-github-event" header value: ${eventName}`,
+				}),
+				{ status: 400, headers: { "content-type": "application/json" } }
+			);
+		}
+
 		const webhooks = new Webhooks({
 			secret: env.WEBHOOK_SECRET,
 		});
 
 		webhooks.on(Object.values(GitHubEvent), handleEvent);
 
-		const middleware = createNodeMiddleware(webhooks, { path: "/events" });
-		const response = new Response();
-		const hasResponse: boolean = await middleware(request, response);
-		return hasResponse ? response : new Response("Not found", { status: 404 });
+		console.debug(`${eventName} event received (id: ${id})`);
+		try {
+			await webhooks.verifyAndReceive({
+				id,
+				name: eventName as EmitterWebhookEventName,
+				payload: await request.text(),
+				signature: signatureSHA256,
+			});
+			return new Response("ok\n", { status: 200, headers: { "content-type": "text/plain" } });
+		} catch (error) {
+			console.error(error);
+			let status = 500;
+			let errorMessage = "An Unspecified error occurred";
+			if (error instanceof AggregateError) {
+				const err = error.errors[0];
+				errorMessage = err.message ? `${err.name}: ${err.message}` : "Error: An Unspecified error occurred";
+				status = typeof err.status !== "undefined" ? err.status : 500;
+			}
+			return new Response(
+				JSON.stringify({
+					error: errorMessage,
+				}),
+				{ status: status, headers: { "content-type": "application/json" } }
+			);
+		}
 	},
 };
