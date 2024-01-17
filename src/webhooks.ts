969cc7ef0129eeb19b2c65f6753cf61593564514
アレクサンダー.eth <gpg@pavlovcik.com>
2024-01-17 15:08:44 +0900

    chore: migrate to bun

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -1,5 +1,29 @@
 import { EmitterWebhookEvent } from "@octokit/webhooks";
+import { makeEventKey } from "./webhooks/make-event-key";
 
 export async function handleEvent(event: EmitterWebhookEvent) {
+  const eventKey = makeEventKey(event);
+
+  console.log(eventKey);
+  console.log();
   console.log(event);
+  console.log();
+  console.log(eventKey);
+
+  switch (eventKey) {
+    case "issues.opened":
+      return notImplemented(event);
+    case "issues.closed":
+      return notImplemented(event);
+    case "issues.reopened":
+      return notImplemented(event);
+    case "pull_request.opened":
+      return notImplemented(event);
+    default:
+      return notImplemented(event);
+  }
+}
+
+function notImplemented(event: EmitterWebhookEvent) {
+  console.log(`Not implemented: ${makeEventKey(event)}`);
 }
--- src/worker.ts
+++ src/worker.ts
@@ -3,100 +3,69 @@ import { Type as T, type Static } from "@sinclair/typebox";
 import { Value } from "@sinclair/typebox/value";
 import { GitHubEvent } from "./types/github-events";
 import { handleEvent } from "./webhooks";
-
-const envSchema = T.Object({
-  WEBHOOK_SECRET: T.String(),
-});
-
+const envSchema = T.Object({ WEBHOOK_SECRET: T.String() });
 type Env = Static<typeof envSchema>;
-
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
     if (!Value.Check(envSchema, env)) {
       const errors = [...Value.Errors(envSchema, env)];
       console.error("Invalid environment variables", errors);
-      return new Response(
-        JSON.stringify({
-          error: "Invalid environment variables",
-        }),
-        { status: 500, headers: { "content-type": "application/json" } }
-      );
+      return new Response(JSON.stringify({ error: "Invalid environment variables" }), {
+        status: 500,
+        headers: { "content-type": "application/json" },
+      });
     }
-
     let pathname;
     try {
       pathname = new URL(request.url, "http://localhost").pathname;
     } catch (error) {
-      return new Response(
-        JSON.stringify({
-          error: `Request URL could not be parsed: ${request.url}`,
-        }),
-        { status: 422, headers: { "content-type": "application/json" } }
-      );
+      return new Response(JSON.stringify({ error: `Request URL could not be parsed: ${request.url}` }), {
+        status: 422,
+        headers: { "content-type": "application/json" },
+      });
     }
-
     if (pathname !== "/events" || request.method !== "POST") {
-      return new Response(
-        JSON.stringify({
-          error: `Unknown route: ${request.method} ${request.url}`,
-        }),
-        { status: 400, headers: { "content-type": "application/json" } }
-      );
+      return new Response(JSON.stringify({ error: `Unknown route: ${request.method} ${request.url}` }), {
+        status: 400,
+        headers: { "content-type": "application/json" },
+      });
     }
-
     if (!request.headers.get("content-type") || !request.headers.get("content-type")?.startsWith("application/json")) {
-      return new Response(
-        JSON.stringify({
-          error: `Unsupported "Content-Type" header value. Must be "application/json"`,
-        }),
-        { status: 415, headers: { "content-type": "application/json" } }
-      );
+      return new Response(JSON.stringify({ error: `Unsupported "Content-Type" header value. Must be "application/json"` }), {
+        status: 415,
+        headers: { "content-type": "application/json" },
+      });
     }
-
     const eventName = request.headers.get("x-github-event");
     if (!eventName) {
-      return new Response(
-        JSON.stringify({
-          error: `Missing "x-github-event" header`,
-        }),
-        { status: 400, headers: { "content-type": "application/json" } }
-      );
+      return new Response(JSON.stringify({ error: `Missing "x-github-event" header` }), {
+        status: 400,
+        headers: { "content-type": "application/json" },
+      });
     }
     const signatureSHA256 = request.headers.get("x-hub-signature-256");
     if (!signatureSHA256) {
-      return new Response(
-        JSON.stringify({
-          error: `Missing "x-hub-signature-256" header`,
-        }),
-        { status: 400, headers: { "content-type": "application/json" } }
-      );
+      return new Response(JSON.stringify({ error: `Missing "x-hub-signature-256" header` }), {
+        status: 400,
+        headers: { "content-type": "application/json" },
+      });
     }
     const id = request.headers.get("x-github-delivery");
     if (!id) {
-      return new Response(
-        JSON.stringify({
-          error: `Missing "x-github-delivery" header`,
-        }),
-        { status: 400, headers: { "content-type": "application/json" } }
-      );
+      return new Response(JSON.stringify({ error: `Missing "x-github-delivery" header` }), {
+        status: 400,
+        headers: { "content-type": "application/json" },
+      });
     }
-
     if (!emitterEventNames.includes(eventName as EmitterWebhookEventName)) {
-      return new Response(
-        JSON.stringify({
-          error: `Unsupported "x-github-event" header value: ${eventName}`,
-        }),
-        { status: 400, headers: { "content-type": "application/json" } }
-      );
+      return new Response(JSON.stringify({ error: `Unsupported "x-github-event" header value: ${eventName}` }), {
+        status: 400,
+        headers: { "content-type": "application/json" },
+      });
     }
-
-    const webhooks = new Webhooks({
-      secret: env.WEBHOOK_SECRET,
-    });
-
+    const webhooks = new Webhooks({ secret: env.WEBHOOK_SECRET });
     webhooks.on(Object.values(GitHubEvent), handleEvent);
-
-    console.debug(`${eventName} event received (id: ${id})`);
+    // console.debug(`${eventName} event received (id: ${id})`);
     try {
       await webhooks.verifyAndReceive({
         id,
@@ -114,12 +83,7 @@ export default {
         errorMessage = err.message ? `${err.name}: ${err.message}` : "Error: An Unspecified error occurred";
         status = typeof err.status !== "undefined" ? err.status : 500;
       }
-      return new Response(
-        JSON.stringify({
-          error: errorMessage,
-        }),
-        { status: status, headers: { "content-type": "application/json" } }
-      );
+      return new Response(JSON.stringify({ error: errorMessage }), { status: status, headers: { "content-type": "application/json" } });
     }
   },
 };
