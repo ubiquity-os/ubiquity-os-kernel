7c7a35ac0a763e732a3b53627f99253b778867db
アレクサンダー.eth <gpg@pavlovcik.com>
2024-01-17 19:33:47 +0900

    chore: type guard seems close to functional

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -1,17 +1,47 @@
 import { EmitterWebhookEvent } from "@octokit/webhooks";
-import { handlers } from "./handlers/handlers";
+import { GitHubEventClassName } from "./types/github-event-class-names";
 import { makeGitHubEventClassName } from "./webhooks/make-github-event-class-name";
+
+type Handlers = {
+  [K in GitHubEventClassName]?: (_payload: EmitterWebhookEvent<K>["payload"]) => Promise<void>;
+};
 
 export async function handleGitHubEvent(event: EmitterWebhookEvent) {
   const gitHubEventKey = makeGitHubEventClassName(event);
-  const handler = handlers[gitHubEventKey];
-  if (handler) {
-    return await handler(event.payload);
+  const isCorrectType = createTypeGuard(gitHubEventKey);
+
+  if (isCorrectType(event.payload)) {
+    const handler = handlers[gitHubEventKey];
+    if (handler) {
+      return await handler(event.payload);
+    } else {
+      return notImplemented(event);
+    }
   } else {
-    return notImplemented(event);
+    console.log(`Payload is not of type ${gitHubEventKey}`);
   }
+
+  // const handler = handlers[gitHubEventKey];
+  // if (handler) {
+  //   return await handler(event.payload);
+  // } else {
+  //   return notImplemented(event);
+  // }
 }
+
+export const handlers: Handlers = {
+  "issue_comment.created": async function issueCommentCreated(payload: EmitterWebhookEvent<"issue_comment.created">["payload"]) {
+    console.log(payload.comment.body);
+  },
+  // Add more handlers here
+};
 
 function notImplemented(event: EmitterWebhookEvent) {
   console.log(`Not implemented: ${event.name}`);
+}
+
+function createTypeGuard<T extends GitHubEventClassName>(eventName: T) {
+  return (payload): payload is EmitterWebhookEvent<T>["payload"] => {
+    return payload.action === eventName.split(".")[1];
+  };
 }
