9e70be557b627c8bef981e728111ad8f88c02428
whilefoo <whilefoo@users.noreply.github.com>
2024-01-21 21:47:56 +0100

    feat: octokit

--- src/handlers/issue/comment_created.ts
+++ src/handlers/issue/comment_created.ts
@@ -2,4 +2,11 @@ import { Context } from "../../context";
 
 export async function handleIssueCommentCreated(event: Context<"issue_comment.created">) {
   console.log(event);
+
+  await event.octokit.issues.createComment({
+    owner: event.payload.repository.owner.login,
+    repo: event.payload.repository.name,
+    issue_number: event.payload.issue.number,
+    body: "Hello from the worker!",
+  });
 }
