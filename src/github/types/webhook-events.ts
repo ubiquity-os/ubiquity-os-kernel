084d1c19d13761d519fa7292122545991c9fef39
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-02-14 01:20:55 +0000

    feat: webhook type

--- src/github/types/webhook-events.ts
+++ src/github/types/webhook-events.ts
@@ -1,2 +1,23 @@
-import { emitterEventNames } from "@octokit/webhooks/dist-types/generated/webhook-names.js";
+import { emitterEventNames } from "@octokit/webhooks";
 export type GitHubEventClassName = (typeof emitterEventNames)[number];
+
+type Formatted<T extends string> = T extends `${infer Prefix}.${infer Rest}` ? `${Prefix}_${Formatted<Rest>}` : T;
+
+type GithubEventWebHookEvents = {
+  [K in Formatted<Uppercase<GitHubEventClassName>>]: Formatted<Lowercase<K>>;
+};
+
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+  // this just spreads the object into a type
+
+  // we need to use {} otherwise it'll type it as an object
+  // eslint-disable-next-line @typescript-eslint/ban-types
+} & {};
+
+export const githubWebhookEvents: Prettify<GithubEventWebHookEvents> = emitterEventNames.reduce((acc: GithubEventWebHookEvents, cur) => {
+  const formatted = cur.replace(/\./g, "_");
+  const upper = formatted.toUpperCase() as Formatted<Uppercase<GitHubEventClassName>>;
+  acc[upper] = formatted.toLowerCase() as Formatted<Extract<GitHubEventClassName, Uppercase<GitHubEventClassName>>>;
+  return acc;
+}, {} as GithubEventWebHookEvents);
