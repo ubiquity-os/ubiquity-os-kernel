387d33b225407efbea20302536b93f920b794e60
whilefoo <whilefoo@users.noreply.github.com>
2024-03-04 09:24:18 +0100

    fix: refactor into smaller functions

--- src/github/utils/config.ts
+++ src/github/utils/config.ts
@@ -33,50 +33,60 @@ export async function getConfig(context: GitHubContext): Promise<Config | null> 
   return config;
 }
 
-// eslint-disable-next-line sonarjs/cognitive-complexity
 function checkPluginChains(config: Config) {
   for (const eventName of eventNames) {
     const plugins = config.plugins[eventName];
     for (const plugin of plugins) {
-      // make sure ids are unique
-      const allIds = new Set();
-      for (const use of plugin.uses) {
-        if (use.id) {
-          if (allIds.has(use.id)) {
-            throw new Error(`Duplicate id ${use.id} in plugin chain`);
-          }
-          allIds.add(use.id);
-        }
-      }
-      // check expressions
-      const calledIds = new Set();
-      for (const use of plugin.uses) {
-        for (const key in use.with) {
-          const value = use.with[key];
-          if (typeof value === "string" && value.match(expressionRegex)) {
-            const matches = value.match(expressionRegex);
-            if (!matches) {
-              throw new Error(`Invalid expression: ${value}`);
-            }
-            const parts = matches[1].split(".");
-            if (parts.length !== 3) {
-              throw new Error(`Invalid expression: ${value}`);
-            }
-            const id = parts[0];
-            if (!allIds.has(id)) {
-              throw new Error(`Expression ${value} refers to non-existent id ${id}`);
-            }
-            if (!calledIds.has(id)) {
-              throw new Error(`Expression ${value} refers to plugin id ${id} before it is called`);
-            }
-            if (parts[1] !== "output") {
-              throw new Error(`Invalid expression: ${value}`);
-            }
-          }
-        }
-        calledIds.add(use.id);
-      }
+      const allIds = checkPluginChainUniqueIds(plugin);
+      checkPluginChainExpressions(plugin, allIds);
     }
+  }
+}
+
+function checkPluginChainUniqueIds(plugin: Config["plugins"]["*"][0]) {
+  const allIds = new Set<string>();
+  for (const use of plugin.uses) {
+    if (!use.id) continue;
+
+    if (allIds.has(use.id)) {
+      throw new Error(`Duplicate id ${use.id} in plugin chain`);
+    }
+    allIds.add(use.id);
+  }
+  return allIds;
+}
+
+function checkPluginChainExpressions(plugin: Config["plugins"]["*"][0], allIds: Set<string>) {
+  const calledIds = new Set<string>();
+  for (const use of plugin.uses) {
+    if (!use.id) continue;
+    for (const key in use.with) {
+      const value = use.with[key];
+      if (typeof value !== "string") continue;
+      checkExpression(value, allIds, calledIds);
+    }
+    calledIds.add(use.id);
+  }
+}
+
+function checkExpression(value: string, allIds: Set<string>, calledIds: Set<string>) {
+  const matches = value.match(expressionRegex);
+  if (!matches) {
+    return;
+  }
+  const parts = matches[1].split(".");
+  if (parts.length !== 3) {
+    throw new Error(`Invalid expression: ${value}`);
+  }
+  const id = parts[0];
+  if (!allIds.has(id)) {
+    throw new Error(`Expression ${value} refers to non-existent id ${id}`);
+  }
+  if (!calledIds.has(id)) {
+    throw new Error(`Expression ${value} refers to plugin id ${id} before it is called`);
+  }
+  if (parts[1] !== "output") {
+    throw new Error(`Invalid expression: ${value}`);
   }
 }
 
