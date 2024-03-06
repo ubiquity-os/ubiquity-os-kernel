745e4a878300da9e3b8be81a2b87e73003f08324
アレクサンダー.eth <gpg@pavlovcik.com>
2024-03-06 16:45:19 +0900

    test: disable loading main module

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,8 +1,8 @@
-import { mainModule } from "../static/main";
+// import { mainModule } from "../static/main";
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it } from "@jest/globals";
 import { db } from "./__mocks__/db";
 import { server } from "./__mocks__/node";
 import usersGet from "./__mocks__/users-get.json";
-import { expect, describe, beforeAll, beforeEach, afterAll, afterEach, it } from "@jest/globals";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
@@ -19,6 +19,6 @@ describe("User tests", () => {
     const res = await fetch("https://api.ubiquity.com/users");
     const data = await res.json();
     expect(data).toMatchObject(usersGet);
-    expect(async () => await mainModule()).not.toThrow();
+    // expect(async () => await mainModule()).not.toThrow();
   });
 });
