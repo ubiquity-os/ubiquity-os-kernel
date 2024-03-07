03c047ec889704cdb5b4fdfc2f95142547b67276
Fernando <fernand.veyrier@epitech.eu>
2024-03-07 16:00:51 +0900

    chore: mock hooks

--- src/github/types/env.ts
+++ src/github/types/env.ts
@@ -2,3 +2,14 @@ import { Type as T, type Static } from "@sinclair/typebox";
 
 export const envSchema = T.Object({ WEBHOOK_SECRET: T.String({ minLength: 1 }), APP_ID: T.String({ minLength: 1 }), PRIVATE_KEY: T.String({ minLength: 1 }) });
 export type Env = Static<typeof envSchema>;
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface ProcessEnv {
+      APP_ID: string;
+      WEBHOOK_SECRET: string;
+      PRIVATE_KEY: string;
+    }
+  }
+}
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,9 +1,52 @@
-import { describe, expect, it, jest } from "@jest/globals";
+import { afterAll, afterEach, beforeAll, describe, expect, it, jest } from "@jest/globals";
+jest.mock("@octokit/webhooks", () => ({
+  ...(jest.requireActual("@octokit/webhooks") as object),
+  Webhooks: WebhooksMocked,
+}));
+
+class WebhooksMocked {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  constructor(_: unknown) {}
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  verifyAndReceive(_: unknown) {
+    return Promise.resolve();
+  }
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  onAny(_: unknown) {}
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  on(_: unknown) {}
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  onError(_: unknown) {}
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  removeListener(_: unknown, __: unknown) {}
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  sign(_: unknown) {}
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  verify(_: unknown, __: unknown) {}
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  receive(_: unknown) {}
+}
+
+import { config } from "dotenv";
 import worker from "../src/worker";
+import { server } from "./__mocks__/node";
+
+config({ path: ".dev.vars" });
+
+beforeAll(() => {
+  server.listen();
+});
+afterEach(() => {
+  server.resetHandlers();
+});
+afterAll(() => {
+  server.close();
+  jest.restoreAllMocks();
+});
 
 describe("Worker tests", () => {
-  it("Should start a worker", async () => {
-    const req = new Request("");
+  it("Should fail on missing env variables", async () => {
+    const req = new Request("http://localhost:8080");
     const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => jest.fn());
     const res = await worker.fetch(req, {
       WEBHOOK_SECRET: "",
@@ -12,5 +55,21 @@ describe("Worker tests", () => {
     });
     expect(res.status).toEqual(500);
     consoleSpy.mockReset();
+  });
+
+  it("Should start a worker", async () => {
+    const req = new Request("http://localhost:8080", {
+      headers: {
+        "x-github-event": "issues.opened",
+        "x-github-delivery": "1",
+        "x-hub-signature-256": "123456",
+      },
+    });
+    const res = await worker.fetch(req, {
+      WEBHOOK_SECRET: process.env.WEBHOOK_SECRET,
+      APP_ID: process.env.APP_ID,
+      PRIVATE_KEY: process.env.PRIVATE_KEY,
+    });
+    expect(res.status).toEqual(200);
   });
 });
