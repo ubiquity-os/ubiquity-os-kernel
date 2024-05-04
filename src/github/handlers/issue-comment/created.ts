45cdc4a708b5ae02bb958dfa0b85d527c00dd475
gentlementlegen <fernand.veyrier@epitech.eu>
2024-05-04 15:43:46 +0900

    chore: removed test log

--- src/github/handlers/issue-comment/created.ts
+++ src/github/handlers/issue-comment/created.ts
@@ -5,11 +5,4 @@ export async function issueCommentCreated(event: GitHubContext<"issue_comment.cr
     console.log("Skipping bot comment");
     return null;
   }
-
-  await event.octokit.issues.createComment({
-    owner: event.payload.repository.owner.login,
-    repo: event.payload.repository.name,
-    issue_number: event.payload.issue.number,
-    body: "Hello from the worker!",
-  });
 }
