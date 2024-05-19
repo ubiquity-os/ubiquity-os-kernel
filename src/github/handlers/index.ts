984c11c9ba6326ccbbe21906b709845ef2315edf
gentlementlegen <fernand.veyrier@epitech.eu>
2024-05-20 02:17:47 +0900

    chore: simplified plugin skipping

--- src/github/handlers/index.ts
+++ src/github/handlers/index.ts
@@ -1,10 +1,11 @@
 import { EmitterWebhookEvent } from "@octokit/webhooks";
+import { GitHubContext } from "../github-context";
 import { GitHubEventHandler } from "../github-event-handler";
 import { getConfig } from "../utils/config";
 import { repositoryDispatch } from "./repository-dispatch";
 import { dispatchWorker, dispatchWorkflow, getDefaultBranch } from "../utils/workflow-dispatch";
 import { DelegatedComputeInputs } from "../types/plugin";
-import { isGithubPlugin } from "../types/plugin-configuration";
+import { isGithubPlugin, PluginConfiguration } from "../types/plugin-configuration";
 
 function tryCatchWrapper(fn: (event: EmitterWebhookEvent) => unknown) {
   return async (event: EmitterWebhookEvent) => {
@@ -19,6 +20,24 @@ function tryCatchWrapper(fn: (event: EmitterWebhookEvent) => unknown) {
 export function bindHandlers(eventHandler: GitHubEventHandler) {
   eventHandler.on("repository_dispatch", repositoryDispatch);
   eventHandler.onAny(tryCatchWrapper((event) => handleEvent(event, eventHandler))); // onAny should also receive GithubContext but the types in octokit/webhooks are weird
+}
+
+function shouldSkipPlugin(event: EmitterWebhookEvent, context: GitHubContext, pluginChain: PluginConfiguration["plugins"]["*"][0]) {
+  if (pluginChain.skipBotEvents && "sender" in event.payload && event.payload.sender?.type === "Bot") {
+    console.log("Skipping plugin chain because sender is a bot");
+    return true;
+  }
+  if (
+    context.key === "issue_comment.created" &&
+    pluginChain.command &&
+    "comment" in context.payload &&
+    typeof context.payload.comment !== "string" &&
+    !context.payload.comment?.body.startsWith(pluginChain.command)
+  ) {
+    console.log("Skipping plugin chain because command does not match");
+    return true;
+  }
+  return false;
 }
 
 async function handleEvent(event: EmitterWebhookEvent, eventHandler: InstanceType<typeof GitHubEventHandler>) {
@@ -44,23 +63,13 @@ async function handleEvent(event: EmitterWebhookEvent, eventHandler: InstanceTyp
   }
 
   for (const pluginChain of pluginChains) {
-    if (pluginChain.skipBotEvents && "sender" in event.payload && event.payload.sender?.type === "Bot") {
-      console.log("Skipping plugin chain because sender is a bot");
-      continue;
-    }
-    if (
-      context.key === "issue_comment.created" &&
-      pluginChain.command &&
-      "comment" in context.payload &&
-      typeof context.payload.comment !== "string" &&
-      !context.payload.comment.body.startsWith(pluginChain.command)
-    ) {
-      console.log("Skipping plugin chain because command does not match");
+    if (shouldSkipPlugin(event, context, pluginChain)) {
       continue;
     }
 
     // invoke the first plugin in the chain
     const { plugin, with: settings } = pluginChain.uses[0];
+    const isGithubPluginObject = isGithubPlugin(plugin);
     console.log(`Calling handler for event ${event.name}`);
 
     const stateId = crypto.randomUUID();
@@ -75,18 +84,16 @@ async function handleEvent(event: EmitterWebhookEvent, eventHandler: InstanceTyp
       inputs: new Array(pluginChain.uses.length),
     };
 
+    const ref = isGithubPluginObject ? plugin.ref ?? (await getDefaultBranch(context, plugin.owner, plugin.repo)) : "";
     const token = await eventHandler.getToken(event.payload.installation.id);
-
-    if (!isGithubPlugin(plugin)) {
-      const inputs = new DelegatedComputeInputs(stateId, context.key, event.payload, settings, token, "");
-      return await dispatchWorker(plugin, inputs.getInputs());
-    }
-
-    const ref = plugin.ref ?? (await getDefaultBranch(context, plugin.owner, plugin.repo));
     const inputs = new DelegatedComputeInputs(stateId, context.key, event.payload, settings, token, ref);
 
     state.inputs[0] = inputs;
     await eventHandler.pluginChainState.put(stateId, state);
+
+    if (!isGithubPluginObject) {
+      return await dispatchWorker(plugin, inputs.getInputs());
+    }
 
     await dispatchWorkflow(context, {
       owner: plugin.owner,
