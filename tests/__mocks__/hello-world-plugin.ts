957f0bb313c5c5b8f4376fc9b09d4a71b65cbcc5
rndquu <rndquu@gmail.com>
2024-06-05 12:49:51 +0300

    feat: add hello-world plugin example

---
+++ tests/__mocks__/hello-world-plugin.ts
@@ -0,0 +1,51 @@
+import { createServer } from "@mswjs/http-middleware";
+import { Octokit } from "@octokit/core";
+import { http, HttpResponse } from "msw";
+
+type KernelInput = {
+  authToken: string;
+  eventPayload: {
+    issue: {
+      number: number;
+    };
+    organization: {
+      login: string;
+    };
+    repository: {
+      name: string;
+    };
+  };
+  settings: {
+    response: string;
+  };
+};
+
+const PORT = 9090;
+
+const handlers = [
+  http.post("/", async (data) => {
+    const body: KernelInput = (await data.request.json()) as KernelInput;
+
+    const octokit = new Octokit({ auth: body.authToken });
+
+    await octokit.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
+      owner: body.eventPayload.organization.login,
+      repo: body.eventPayload.repository.name,
+      issue_number: body.eventPayload.issue.number,
+      body: body.settings.response,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+
+    return HttpResponse.json({
+      state_id: "state_id_uuid_1",
+      output: `{ "result": "success", "message": "${body.settings.response}" }`,
+    });
+  }),
+];
+
+const httpServer = createServer(...handlers);
+
+httpServer.listen(PORT);
+console.log(`hello-world-plugin is listening on http://127.0.0.1:${PORT}`);
