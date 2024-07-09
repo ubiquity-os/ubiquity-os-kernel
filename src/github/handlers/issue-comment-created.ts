82e2c652d888827c4dda8de8eeb3168a7dadfca0
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-07-09 17:12:04 +0900

    chore: help for plugins (WIP)

--- src/github/handlers/issue-comment-created.ts
+++ src/github/handlers/issue-comment-created.ts
@@ -1,5 +1,6 @@
 import { GitHubContext } from "../github-context";
 import { getConfig } from "../utils/config";
+import { GithubPlugin, isGithubPlugin } from "../types/plugin-configuration";
 
 export default async function issueCommentCreated(context: GitHubContext<"issue_comment.created">) {
   const body = context.payload.comment.body.trim();
@@ -12,11 +13,17 @@ export default async function issueCommentCreated(context: GitHubContext<"issue_
     ];
     const configuration = await getConfig(context);
     for (const pluginArray of Object.values(configuration.plugins)) {
-      for (const plugin of pluginArray) {
-        // Only show plugins that have commands available for the user
-        if (plugin.command) {
-          comments.push(`| \`${getContent(plugin.command)}\` | ${getContent(plugin.description)} | \`${getContent(plugin.example)}\` |`);
+      for (const pluginElement of pluginArray) {
+        const { plugin } = pluginElement.uses[0];
+        if (isGithubPlugin(plugin)) {
+          console.log("Github plugin", await fetchActionManifest(context, plugin));
+        } else {
+          console.log("Worker plugin", await fetchWorkerManifest(plugin));
         }
+        // Only show plugins that have commands available for the user
+        // if (plugin.command) {
+        //   comments.push(`| \`${getContent(plugin.command)}\` | ${getContent(plugin.description)} | \`${getContent(plugin.example)}\` |`);
+        // }
       }
     }
     await context.octokit.issues.createComment({
@@ -31,6 +38,33 @@ export default async function issueCommentCreated(context: GitHubContext<"issue_
 /**
  * Ensures that passed content does not break MD display within the table.
  */
-function getContent(content: string | undefined) {
-  return content ? content.replace("|", "\\|") : "-";
+// function getContent(content: string | undefined) {
+//   return content ? content.replace("|", "\\|") : "-";
+// }
+
+async function fetchActionManifest(context: GitHubContext<"issue_comment.created">, { owner, repo }: GithubPlugin) {
+  try {
+    const { data } = await context.octokit.repos.getContent({
+      owner,
+      repo,
+      path: "manifest.json",
+    });
+    if ("content" in data) {
+      const content = Buffer.from(data.content, "base64").toString();
+      return JSON.parse(content);
+    }
+  } catch (e) {
+    console.warn(`Could not find a manifest for ${owner}/${repo}`);
+  }
+  return null;
+}
+
+async function fetchWorkerManifest(url: string) {
+  try {
+    const { json } = await fetch(`${url}/manifest.json`);
+    return await json();
+  } catch (e) {
+    console.warn(`Could not find a manifest for ${url}`);
+  }
+  return null;
 }
