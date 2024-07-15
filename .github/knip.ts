026ed429e5ea1ca164d46a138042cb26e8f3b259
whilefoo <whilefoo@users.noreply.github.com>
2024-07-15 20:32:52 +0200

    fix: ci

--- .github/knip.ts
+++ .github/knip.ts
@@ -1,8 +1,9 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-  entry: ["src/worker.ts"],
+  entry: ["src/worker.ts", "deploy/setup-kv-namespace.ts"],
   project: ["src/**/*.ts"],
+  ignore: ["jest.config.js"],
   ignoreBinaries: ["i"],
   ignoreExportsUsedInFile: true,
   ignoreDependencies: ["@mswjs/data", "esbuild", "eslint-config-prettier", "eslint-plugin-prettier", "msw"],
