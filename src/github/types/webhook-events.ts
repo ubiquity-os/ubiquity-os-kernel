6136da5760c7bf88caf95e47b5606991f3c29a47
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-07-18 12:26:20 +0900

    chore: deleted unused file webhook-events.ts

--- src/github/types/webhook-events.ts
+++
@@ -1,4 +0,0 @@
-import { emitterEventNames, EmitterWebhookEventName as GitHubEventClassName } from "@octokit/webhooks";
-
-export type EventName = GitHubEventClassName;
-export const eventNames: EventName[] = [...emitterEventNames];
