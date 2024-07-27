f56714b24474400f82e1e53026d4cfd600549091
whilefoo <whilefoo@users.noreply.github.com>
2024-07-27 18:22:13 +0200

    feat: export

--- tsup.config.js
+++ tsup.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/sdk/index.ts"],
+  format: ["cjs", "esm"],
+  outDir: "dist",
   splitting: false,
   sourcemap: true,
   clean: true,
