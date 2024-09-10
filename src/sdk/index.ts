d292f5abf93301aaee0d508a0166d891ac36fd18
whilefoo <whilefoo@users.noreply.github.com>
2024-09-10 13:02:36 +0200

    feat: add app id and bot user id

--- src/sdk/index.ts
+++ src/sdk/index.ts
@@ -1,4 +1,4 @@
 export { createPlugin } from "./server";
 export { createActionsPlugin } from "./actions";
 export type { Context } from "./context";
-export { KERNEL_APP_ID, KERNEL_PUBLIC_KEY } from "./constants";
+export * from "./constants";
