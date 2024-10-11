f30d3610b7ac57c0c2bd9992a53cbbd7b0b93c5a
gentlementlegen <fernand.veyrier@epitech.eu>
2024-10-11 10:44:05 +0900

    refactor: remove custom Husky install script
    Deleted custom Husky install script and updated build process.

--- .husky/install.mjs
+++
@@ -1,8 +0,0 @@
-// Skip Husky install in production and CI
-// https://typicode.github.io/husky/how-to.html#ci-server-and-docker
-if (process.env.NODE_ENV === "production" || process.env.CI === "true") {
-  console.log("Skipping Husky install because of production environment.");
-  process.exit(0);
-}
-const husky = (await import("husky")).default;
-console.log(husky());
