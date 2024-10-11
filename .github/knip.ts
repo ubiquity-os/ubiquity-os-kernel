313a3ea88964336d37fcab27027a5011acba4998
gentlementlegen <fernand.veyrier@epitech.eu>
2024-10-11 10:11:23 +0900

    chore(knip): update ignoreBinaries list
    Added 'publish' to the ignoreBinaries list in knip config.

--- .github/knip.ts
+++ .github/knip.ts
@@ -4,7 +4,7 @@ const config: KnipConfig = {
   entry: ["src/worker.ts", "deploy/setup-kv-namespace.ts"],
   project: ["src/**/*.ts"],
   ignore: ["jest.config.ts"],
-  ignoreBinaries: ["i"],
+  ignoreBinaries: ["i", "publish"],
   ignoreExportsUsedInFile: true,
   ignoreDependencies: ["@mswjs/data", "esbuild", "eslint-config-prettier", "eslint-plugin-prettier", "msw", "ts-node"],
 };
