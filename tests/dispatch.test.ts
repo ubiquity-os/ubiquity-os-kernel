be6626c7fd9d6621d7ba04c9c8544502a9e80af2
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-10-19 11:52:53 +0900

    test: enhance mocks and add failure handling in dispatch tests
    Adjusted mocks for several modules and introduced failure handling for improved test robustness.

--- tests/dispatch.test.ts
+++ tests/dispatch.test.ts
@@ -8,7 +8,37 @@ jest.mock("@octokit/plugin-paginate-rest", () => ({}));
 jest.mock("@octokit/plugin-rest-endpoint-methods", () => ({}));
 jest.mock("@octokit/plugin-retry", () => ({}));
 jest.mock("@octokit/plugin-throttling", () => ({}));
-jest.mock("@octokit/auth-app", () => ({}));
+jest.mock("@octokit/auth-app", () => ({
+  createAppAuth: jest.fn(() => () => jest.fn(() => "1234")),
+}));
+
+jest.mock("../src/github/utils/cloudflare-kv", () => ({
+  CloudflareKv: jest.fn().mockImplementation(() => ({
+    get: jest.fn(),
+    put: jest.fn(),
+  })),
+}));
+
+jest.mock("../src/github/types/plugin", () => {
+  const originalModule: typeof import("../src/github/types/plugin") = jest.requireActual("../src/github/types/plugin");
+
+  return {
+    ...originalModule,
+    PluginInput: class extends originalModule.PluginInput {
+      async getWorkerInputs() {
+        return {
+          stateId: this.stateId,
+          eventName: this.eventName,
+          eventPayload: this.eventPayload,
+          settings: this.settings,
+          authToken: this.authToken,
+          ref: this.ref,
+          signature: "",
+        };
+      }
+    },
+  };
+});
 
 function calculateSignature(payload: string, secret: string) {
   return `sha256=${crypto.createHmac("sha256", secret).update(payload).digest("hex")}`;
@@ -30,6 +60,7 @@ describe("handleEvent", () => {
       http.get("https://plugin-a.internal/manifest.json", () =>
         HttpResponse.json({
           name: "plugin",
+          "ubiquity:listeners": ["issue_comment.created"],
           commands: {
             foo: {
               description: "foo command",
@@ -42,21 +73,27 @@ describe("handleEvent", () => {
           },
         })
       ),
-      http.get("https://api.github.com/repos/test-user/.ubiquity-os/contents/.github%2F.ubiquity-os.config.yml", () =>
-        HttpResponse.json({
-          type: "file",
-          encoding: "base64",
-          size: 536,
-          name: ".ubiquity-os.config.yml",
-          path: ".github/.ubiquity-os.config.yml",
-          content: Buffer.from(`plugins:\n  - uses:\n    - plugin: "https://plugin-a.internal"`).toString("base64"),
-          sha: "3ffce0fe837a21b1237acd38f7b1c3d2f7d73656",
-          url: "https://api.github.com/repos/test-user/.ubiquity-os/contents/.github%2F.ubiquity-os.config.yml",
-          git_url: "https://api.github.com/repos/test-user/.ubiquity-os/git/blobs/3ffce0fe837a21b1237acd38f7b1c3d2f7d73656",
-          html_url: "https://github.com/test-user/.ubiquity-os/blob/main/.github/.ubiquity-os.config.yml",
-          download_url: "https://raw.githubusercontent.com/test-user/.ubiquity-os/main/.github/.ubiquity-os.config.yml",
-        })
-      )
+      http.get("https://api.github.com/repos/test-user/.ubiquity-os/contents/.github%2F.ubiquity-os.config.yml", (req) => {
+        const acceptHeader = req.request.headers.get("accept");
+        const yamlContent = `plugins:\n  - uses:\n    - plugin: "https://plugin-a.internal"\n  - uses:\n    - plugin: "https://plugin-a.internal"`;
+        if (acceptHeader === "application/vnd.github.v3.raw") {
+          return HttpResponse.text(yamlContent);
+        } else {
+          return HttpResponse.json({
+            type: "file",
+            encoding: "base64",
+            size: 62,
+            name: ".ubiquity-os.config.yml",
+            path: ".github/.ubiquity-os.config.yml",
+            content: Buffer.from(yamlContent).toString("base64"),
+            sha: "3ffce0fe837a21b1237acd38f7b1c3d2f7d73656",
+            url: "https://api.github.com/repos/test-user/.ubiquity-os/contents/.github%2F.ubiquity-os.config.yml",
+            git_url: "https://api.github.com/repos/test-user/.ubiquity-os/git/blobs/3ffce0fe837a21b1237acd38f7b1c3d2f7d73656",
+            html_url: "https://github.com/test-user/.ubiquity-os/blob/main/.github/.ubiquity-os.config.yml",
+            download_url: "https://raw.githubusercontent.com/test-user/.ubiquity-os/main/.github/.ubiquity-os.config.yml",
+          });
+        }
+      })
     );
   });
 
@@ -66,6 +103,16 @@ describe("handleEvent", () => {
         customOctokit: jest.fn().mockReturnValue(new Octokit()),
       };
     });
+    const dispatchWorker = jest
+      .fn()
+      .mockImplementationOnce(() => {
+        throw new Error("Test induced first call failure");
+      })
+      .mockImplementationOnce(() => Promise.resolve("success"));
+    jest.mock("../src/github/utils/workflow-dispatch", () => ({
+      ...(jest.requireActual("../src/github/utils/workflow-dispatch") as object),
+      dispatchWorker: dispatchWorker,
+    }));
     const payload = {
       installation: {
         id: 1,
@@ -111,5 +158,7 @@ describe("handleEvent", () => {
     });
 
     expect(res).toBeTruthy();
+    // 2 calls means the execution didn't break
+    expect(dispatchWorker).toHaveBeenCalledTimes(2);
   });
 });
