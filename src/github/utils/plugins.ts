84e4c7f1192cf46ad1c9b8149d4622886a53d48c
gentlementlegen <fernand.veyrier@epitech.eu>
2024-12-02 16:46:49 +0900

    chore: restore manifest cache

--- src/github/utils/plugins.ts
+++ src/github/utils/plugins.ts
@@ -5,6 +5,8 @@ import { Manifest, manifestSchema } from "@ubiquity-os/plugin-sdk/manifest";
 import { Buffer } from "node:buffer";
 import { Value } from "@sinclair/typebox/value";
 
+const _manifestCache: Record<string, Manifest> = {};
+
 export function getPluginsForEvent(plugins: PluginConfiguration["plugins"], event: EmitterWebhookEventName) {
   return plugins.filter((plugin) => {
     return plugin.uses?.[0].runsOn?.includes(event);
@@ -16,6 +18,10 @@ export function getManifest(context: GitHubContext, plugin: string | GithubPlugi
 }
 
 async function fetchActionManifest(context: GitHubContext<"issue_comment.created">, { owner, repo, ref }: GithubPlugin): Promise<Manifest | null> {
+  const manifestKey = ref ? `${owner}:${repo}:${ref}` : `${owner}:${repo}`;
+  if (_manifestCache[manifestKey]) {
+    return _manifestCache[manifestKey];
+  }
   try {
     const { data } = await context.octokit.rest.repos.getContent({
       owner,
@@ -26,7 +32,9 @@ async function fetchActionManifest(context: GitHubContext<"issue_comment.created
     if ("content" in data) {
       const content = Buffer.from(data.content, "base64").toString();
       const contentParsed = JSON.parse(content);
-      return decodeManifest(contentParsed);
+      const manifest = decodeManifest(contentParsed);
+      _manifestCache[manifestKey] = manifest;
+      return manifest;
     }
   } catch (e) {
     console.warn(`Could not find a manifest for Action ${owner}/${repo}: ${e}`);
@@ -35,11 +43,16 @@ async function fetchActionManifest(context: GitHubContext<"issue_comment.created
 }
 
 async function fetchWorkerManifest(url: string): Promise<Manifest | null> {
+  if (_manifestCache[url]) {
+    return _manifestCache[url];
+  }
   const manifestUrl = `${url}/manifest.json`;
   try {
     const result = await fetch(manifestUrl);
     const jsonData = await result.json();
-    return decodeManifest(jsonData);
+    const manifest = decodeManifest(jsonData);
+    _manifestCache[url] = manifest;
+    return manifest;
   } catch (e) {
     console.warn(`Could not find a manifest for Worker ${manifestUrl}: ${e}`);
   }
