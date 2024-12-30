8ff05d4693fd127ce4e0d1e1e8f2efe5d86cae86
gentlementlegen <fernand.veyrier@epitech.eu>
2024-12-30 16:38:25 +0900

    chore: added req log

--- src/github/utils/config.ts
+++ src/github/utils/config.ts
@@ -76,6 +76,8 @@ export async function getConfig(context: GitHubContext): Promise<PluginConfigura
 
   checkPluginChains(mergedConfiguration);
 
+  console.log(`Found ${mergedConfiguration.plugins.length} configurations for ${payload.repository.owner.login}/${payload.repository.name}`);
+
   for (const plugin of mergedConfiguration.plugins) {
     const manifest = await getManifest(context, plugin.uses[0].plugin);
     if (manifest) {
