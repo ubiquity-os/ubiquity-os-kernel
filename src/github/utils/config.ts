127679faf1da17f7c548487697c08a694172e199
gentlementlegen <fernand.veyrier@epitech.eu>
2025-01-18 12:16:41 +0900

    fix: the default values for skipBotEvents and ubiquity:listeners is no longer overridden

--- src/github/utils/config.ts
+++ src/github/utils/config.ts
@@ -92,9 +92,8 @@ export async function getConfig(context: GitHubContext): Promise<PluginConfigura
 
   for (const plugin of mergedConfiguration.plugins) {
     const manifest = await getManifest(context, plugin.uses[0].plugin);
-    if (manifest) {
+    if (manifest && !plugin.uses[0].runsOn.length) {
       plugin.uses[0].runsOn = manifest["ubiquity:listeners"] ?? [];
-      plugin.uses[0].skipBotEvents = manifest.skipBotEvents ?? true;
     }
   }
   return mergedConfiguration;
