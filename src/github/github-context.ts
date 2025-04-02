ed5c7277a6a1be44b51b7cffe98e6fcd916ee496
shiv810 <test@example.com>
2025-04-02 14:31:35 -0400

    fix: types

--- src/github/github-context.ts
+++ src/github/github-context.ts
@@ -3,7 +3,7 @@ import { customOctokit } from "./github-client";
 import { GitHubEventHandler } from "./github-event-handler";
 import OpenAI from "openai";
 import { Logs, LOG_LEVEL } from "@ubiquity-os/ubiquity-os-logger";
-import { CommentHandler, Context } from "@ubiquity-os/plugin-sdk";
+import { CommentHandler } from "@ubiquity-os/plugin-sdk";
 import { VoyageAIClient } from "voyageai";
 
 export class GitHubContext<TSupportedEvents extends WebhookEventName = WebhookEventName> {
@@ -18,7 +18,7 @@ export class GitHubContext<TSupportedEvents extends WebhookEventName = WebhookEv
   public openAi: OpenAI;
   public voyageAiClient: VoyageAIClient;
   public logger: Logs;
-  public commentHandler: Context["commentHandler"];
+  public commentHandler: CommentHandler;
 
   constructor(
     eventHandler: InstanceType<typeof GitHubEventHandler>,
